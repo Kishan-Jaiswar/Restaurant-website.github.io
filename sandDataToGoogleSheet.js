1c14cd72b42f4dc23fed51ea51e4bbb15b0b3c2f
Kishan-Jaiswar <134482768+Kishan-Jaiswar@users.noreply.github.com>
2023-05-24 12:49:14 +0530

    Add files via upload

---
+++ sandDataToGoogleSheet.js
@@ -0,0 +1,13 @@
+var form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        Response => Response.json()
+    ).then((html) => {
+        //if you any to open any page after submitting the form
+        //window.open("Thanks.html", "_blank");
+    });
+});
